e88be85075bafc1674f39095418718a1b27e0051
sahilghadi47 <ghadigaonkarsahil47@gmail.com>
2024-04-24 09:52:33 +0530

    update

--- Introduction/Guess_Number/script.js
+++ Introduction/Guess_Number/script.js
@@ -10,27 +10,26 @@ let count =0;
 restart.style.display ="none";
 
 btn.addEventListener('click',() =>{
-    // let num = Math.floor(Math.random() * 100);
-    let num = 47;
+    let num = Math.floor(Math.random() * 100);
+    // let num =47;
     let input = userInput.value;
-    
     const txt_area1 = document.createElement('p');
-    txt_area1.textContent= `You Entered: ${input}`;
-    const txt_area2 = document.createElement('p');
-    txt_area2.textContent= `Number Was: ${num}`;
-    demo.appendChild(txt_area1);
-    demo.appendChild(txt_area2);
-    if(num !=input){
-        res.innerHTML = `Wrong Answer !`;
-        res.style.backgroundColor = 'red';
+    if(input === ''){
+        txt_area1.textContent= `Please Enter a number `;
+        demo.appendChild(txt_area1);
     }else{
-        res.innerHTML = `Right Answer !`;
-        res.style.backgroundColor = 'green';
+        txt_area1.textContent= `You Entered: ${input} and Num was: ${num} `;
+        demo.appendChild(txt_area1);
+        if(num != input){
+            res.innerHTML = `Wrong Answer !`;
+            res.style.backgroundColor = 'red';
+        }else{
+            res.innerHTML = `Right Answer !`;
+            res.style.backgroundColor = 'green';
+        }
+        count++;
     }
-    console.log(num);
-    console.log(input);
-    console.log(num == input);
-    count++;
+    console.log(count)
     if(count >=10){
         input.disabled = true;
         btn.disabled   = true;
@@ -44,9 +43,10 @@ restart.addEventListener('click', restartGame);
 
 function restartGame() {
     input.disabled = false;
-    btn.disabled   =false;
-    temp.innerHTML ='' ;
-    demo.innerHTML='';
+    btn.disabled   = false;
+    res.innerHTML = ''
+    res.style.backgroundColor = 'white'
+    demo.innerText=''
     count =0;
     restart.style.display = "none";
 }
