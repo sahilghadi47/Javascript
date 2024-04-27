ffe40513e0f1e5288d974e5cac9121786ad64d9c
sahilghadi47 <ghadigaonkarsahil47@gmail.com>
2024-04-27 17:47:24 +0530

    create to do list app in javascript

--- Introduction/to_do_list/main.js
+++ Introduction/to_do_list/main.js
@@ -1,1 +1,48 @@
+
+const myList = document.getElementById('myList');
+const input = document.getElementById('title');
+const msg = document.getElementById('msg');
+const submit = document.getElementById('submit');
+
+
+// add new task;
+submit.addEventListener('click',() =>{
+    
+    if(input.value === ''){
+        msg.style.display ='block'
+        msg.innerHTML='Title cannont be empty';
+    }else{
+        const li = document.createElement('li')
+        li.classList.add('listItem');
+        
+        const trash = document.createElement('i');
+        trash.classList.add('fa-solid');
+        trash.classList.add('fa-trash');
+
+        li.innerText= input.value;
+        li.appendChild(trash);
+        myList.appendChild(li);
+        msg.style.display= 'none';
+        
+        // remove list item on click trash icon
+        
+        trash.addEventListener('click' ,(event) =>{
+            if(event.target.tagName =='I'){
+                let child = event.target.parentElement;
+                myList.removeChild(child);
+            }
+        })
+    }
+    input.value='';
+})
+
+// change task value  
+myList.addEventListener('click', (ev) => {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('done');
+    }
+}, false)
+
+
+console.log(myList);
 console.log('Running');
